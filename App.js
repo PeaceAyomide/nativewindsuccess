01e560be8e7f882c7fc0bab444b6d5f7d1668dbd
Peace Ayomide <helaralex2000@gmail.com>
2025-02-08 13:58:55 +0100

    new

--- App.js
+++ App.js
@@ -1,20 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {  Text, View, TouchableOpacity } from 'react-native';
+import './global.css'; 
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View className="flex-1 justify-center items-center bg-black gap-4">
+      <Text className=" text-white text-[30px] font-bold"> Goal </Text>
+      <Text className="text-white"> Make my Tailwind Work</Text>
+   <TouchableOpacity className="bg-blue-500 p-5 rounded-[20px]">
+        <Text className="text-white">Press me</Text>
+   </TouchableOpacity>
       <StatusBar style="auto" />
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  // Include paths to all your component files
+  content: [
+    "./App.js", // Main entry point
+    "./components/**/*.{js,jsx}", // All JS/JSX files in components folder
+    // Add other directories if you have more components
+  ],
+  presets: [require("nativewind/preset")],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
